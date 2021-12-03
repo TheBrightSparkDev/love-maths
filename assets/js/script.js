acd6215596ac388ae4fc39662897c526c74a363d
TheBrightSparkDev <danharriesxd@gmail.com>
2021-12-03 01:01:21 +0000

    feat - added addition functionality in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,27 @@ document.addEventListener("DOMContentLoaded",function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                // alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
-}
-)
+    runGame("addition");
+})
 /**
  * generates two random numbers so that the user will see the question they
  * have to answer.
  */
-function runGame(){
-    let num1 = Math.floor(math.random()*25) + 1;
-    let num2 = Math.floor(math.random()*25) + 1;
+function runGame(gameType){
+    
+    let num1 = Math.floor(Math.random()*25) + 1;
+    let num2 = Math.floor(Math.random()*25) + 1;
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    }else {
+        alert(`unknown game type: ${gameType}`)
+        throw(`unknown game type: ${gameType} Aborting!`)
+    }
 }
 function checkAnswer(){
 
@@ -33,8 +41,10 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1,operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 function displaySubtractQuestion(){
     
