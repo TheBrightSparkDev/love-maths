b0c0f5a5f7cabec7b54496f6e33b9b4ce2b6bf37
TheBrightSparkDev <danharriesxd@gmail.com>
2021-12-03 01:31:14 +0000

    feat - addition game now works left to implement are subtract, multiply and divide

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded",function() {
     for (let button of buttons){
         button.addEventListener("click",function(){
             if (this.getAttribute("data-type")==="submit"){
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 // alert(`You clicked ${gameType}`);
@@ -29,13 +29,22 @@ function runGame(gameType){
         throw(`unknown game type: ${gameType} Aborting!`)
     }
 }
+/**
+ * checks if the answer is correct.
+ */
 function checkAnswer(){
-   
+   let userAnswer = parseInt(document.getElementById("answer-box").value);
+   let calculatedAnswer = calculateCorrectAnswer();
+   let isCorrect = userAnswer === calculatedAnswer[0];
+   if (isCorrect){
+       alert("You got it right!")
+   } else alert(`aww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+   runGame(calculatedAnswer[1]);
 }
 function calculateCorrectAnswer(){
-    let operand1 = parseInt(getElementById("operand1").innerHtml);
-    let operator = getElementById("operator").innerHtml;
-    let operand2 = parseInt(getElementById("operand2").innerHtml);
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operator = document.getElementById("operator").innerText;
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
     if (operator === "+"){
         return [operand1 + operand2, "addition"]
     } else {
