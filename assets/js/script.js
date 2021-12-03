8ae6a06ffeffb639f7efc764ebbebbe7b3e4f530
TheBrightSparkDev <danharriesxd@gmail.com>
2021-12-03 00:45:48 +0000

    feat - added two functions to java script runGame() and function that adds all event listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded",function() {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons){
+        button.addEventListener("click",function(){
+            if (this.getAttribute("data-type")==="submit"){
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+}
+)
+/**
+ * generates two random numbers so that the user will see the question they
+ * have to answer.
+ */
+function runGame(){
+    let num1 = Math.floor(math.random()*25) + 1;
+    let num2 = Math.floor(math.random()*25) + 1;
 }
 function checkAnswer(){
 
